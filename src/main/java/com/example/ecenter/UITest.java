649a5c577660aca841405f302d94ba8289a8b96d
Grim <mirgantrophy@gmail.com>
2017-04-20 15:10:44 -0500

    updated login view

---
+++ src/main/java/com/example/ecenter/UITest.java
@@ -0,0 +1,30 @@
+package com.example.ecenter;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+@Theme("valo")
+@Title("Electrolysis Center")
+//@Widgetset("com.vaadin.demo.dashboard.DashboardWidgetSet")
+public class UITest extends UI 
+{
+
+	@Override
+	protected void init(VaadinRequest request) 
+	{
+		updateContent();
+		
+	}
+
+	private void updateContent() 
+	{
+		setContent(new LoginView());
+		
+	}
+
+}
